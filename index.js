005c65422bd5488dcc3a1cd27c570ad409e4d1f4
Jeffrey Au <72361838+jeffreyaupc@users.noreply.github.com>
2020-11-24 20:36:42 -0500

    Update index.js
    added JS for switching tabs when clicking corresponding buttons.

--- index.js
+++ index.js
@@ -1,7 +1,90 @@
 var blinkEle = document.getElementById("btn-notice");
 var blinkHtml = document.getElementById("noticeMsg");
 var blinkIcon = document.getElementById("noticeIcon");
-var mainContainer = document.getElementById("mainContainer");
+var outerContainer = document.getElementById("outerContainer");
+var tabProfile = document.getElementById("tab-profile");
+var tabJS = document.getElementById("tab-js-app");
+var tabResWeb = document.getElementById("tab-res-web");
+var contentProfile = document.getElementById("contentProfile");
+var profileOne = document.getElementById("profileOne");
+var profileTwo = document.getElementById("profileTwo");
+var jsOne = document.getElementById("jsOne");
+var jsTwo = document.getElementById("jsTwo");
+var resWebOne = document.getElementById("resWebOne");
+var resWebTwo = document.getElementById("resWebTwo");
+
+// *****trying to make code of switching tabs cleaner by making tabs as a parameter
+//      when entered, switch to appropriate tab, but failed******
+
+// var btn = tabProfile || tabJS || tabResWeb;
+
+// btn.addEventListener("click", function (e) {
+//   if ((e = tabProfile)) {
+//     toProfile();
+//   } else if ((e = tabJS)) {
+//     toJS();
+//   } else if ((e = tabResWeb)) {
+//     toResWeb();
+//   }
+// });
+
+tabProfile.addEventListener("click", toProfile);
+tabJS.addEventListener("click", toJS);
+tabResWeb.addEventListener("click", toResWeb);
+
+function toJS() {
+  tabJS.classList.add("active");
+  tabProfile.classList.remove("active");
+  tabResWeb.classList.remove("active");
+  jsOne.style.display = "block";
+  jsTwo.style.display = "block";
+  profileOne.style.display = "none";
+  profileTwo.style.display = "none";
+  resWebOne.style.display = "none";
+  resWebTwo.style.display = "none";
+  // jsOne.style.opacity = "1"
+  // jsTwo.style.opacity = "1";
+  // profileOne.style.opacity = "0";
+  // profileTwo.style.opacity = "0";
+  // resWebOne.style.opacity = "0";
+  // resWebTwo.style.opacity = "0";
+}
+
+function toResWeb() {
+  tabResWeb.classList.add("active");
+  tabProfile.classList.remove("active");
+  tabJS.classList.remove("active");
+  resWebOne.style.display = "block";
+  resWebTwo.style.display = "block";
+  profileOne.style.display = "none";
+  profileTwo.style.display = "none";
+  jsOne.style.display = "none";
+  jsTwo.style.display = "none";
+  // resWebOne.style.opacity = "1";
+  // resWebTwo.style.opacity = "1";
+  // jsOne.style.opacity = "0";
+  // jsTwo.style.opacity = "0";
+  // profileOne.style.opacity = "0";
+  // profileTwo.style.opacity = "0";
+}
+
+function toProfile() {
+  tabProfile.classList.add("active");
+  tabResWeb.classList.remove("active");
+  tabJS.classList.remove("active");
+  profileOne.style.display = "block";
+  profileTwo.style.display = "block";
+  jsOne.style.display = "none";
+  jsTwo.style.display = "none";
+  resWebOne.style.display = "none";
+  resWebTwo.style.display = "none";
+  // profileOne.style.opacity = "1";
+  // profileTwo.style.opacity = "1";
+  // resWebOne.style.opacity = "0";
+  // resWebTwo.style.opacity = "0";
+  // jsOne.style.opacity = "0";
+  // jsTwo.style.opacity = "0";
+}
 
 function blink() {
   blinkEle.style.opacity = blinkEle.style.opacity == "0" ? "1" : "0";
@@ -10,7 +93,7 @@ function blink() {
 var blinkblink = setInterval(blink, 800);
 
 function showProfile() {
-  mainContainer.style.opacity = "1";
+  outerContainer.style.opacity = "1";
 }
 
 function clickMsg() {
@@ -21,7 +104,7 @@ function clickMsg() {
   blinkIcon.style.marginTop = "-0.5rem";
   blinkIcon.style.color = "grey";
   blinkEle.style.color = "grey";
-  mainContainer.style.opacity = "1";
+  outerContainer.style.opacity = "1";
 }
 
 blinkEle.addEventListener("click", clickMsg);
